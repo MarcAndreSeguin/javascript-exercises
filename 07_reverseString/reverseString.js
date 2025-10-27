d556e1e349af4d5e994aea6d9ab69973d75b709b
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-10-26 20:21:38 -0400

    Completed Ex7 on reverse strings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    returnString = ''
 
+    for (let i = str.length-1; i >= 0; i--) {
+        returnString += str[i]
+    }
+
+    return returnString
+
+    //solution simply was:
+    // return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
