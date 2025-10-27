3fa5007c2a3a8e8ce270e55338c011ef12057a38
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-10-26 20:16:39 -0400

    Completed Ex6 on repeated strings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, frequency) {
+    if (frequency < 0) return "ERROR";
+    returnString = ''
+    for (let i = 0; i < frequency; i ++) {
+        returnString += word
+    }
+    return returnString
 };
 
 // Do not edit below this line
