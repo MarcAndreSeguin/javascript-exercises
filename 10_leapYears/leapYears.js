34bad1842aad4243ec862e28946c8ad7f95af1ae
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-11-07 21:00:23 -0500

    Completed Ex10 on leap years conditionals

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisibleByFour = year % 4 === 0;
+    isCentury = year % 100 === 0;
+    divisibleByFourHundred = year % 400 === 0;
 
+    if (divisibleByFour && (!isCentury || divisibleByFourHundred)) return true;
+
+    return false;
 };
 
 // Do not edit below this line
